3a908f9cebb895d2ac4ae97bb211e0745e4c5245
Angel Ponce <acdpf000@gmail.com>
2023-01-25 09:21:15 -0600

    search queens

--- src/Board.tsx
+++ src/Board.tsx
@@ -4,6 +4,10 @@ import Tile from "./Tile";
 const Board: FC<{
   solution: number[][];
 }> = ({ solution }) => {
+  const tileHasQueen = (j: number, k: number) => {
+    return solution.some(([peer1, peer2]) => peer1 === j && peer2 === k);
+  };
+
   return (
     <div className="overflow-auto w-full flex justify-center">
       <div className="grid grid-cols-[repeat(8,_56px)] w-auto h-auto">
@@ -12,7 +16,14 @@ const Board: FC<{
           .map((_, j) =>
             new Array(8)
               .fill({})
-              .map((_, k) => <Tile key={`${j}${k}`} j={j + 1} k={k + 1}></Tile>)
+              .map((_, k) => (
+                <Tile
+                  key={`${j}${k}`}
+                  j={j + 1}
+                  k={k + 1}
+                  hasQueen={tileHasQueen(j + 1, k + 1)}
+                />
+              ))
           )}
       </div>
     </div>
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,16 +1,25 @@
 import clsx from "clsx";
 import { FC } from "react";
 
-const Tile: FC<{ j: number; k: number }> = ({ j, k }) => {
+const Tile: FC<{ j: number; k: number; hasQueen: boolean }> = ({
+  j,
+  k,
+  hasQueen,
+}) => {
   return (
     <div
       className={clsx(
-        "min-w-[56px] min-h-[56px] w-14 h-14 border",
+        "relative min-w-[56px] min-h-[56px] w-14 h-14 border flex justify-center items-center",
         j % 2 === 0
           ? k % 2 === 0 && "bg-gray-200"
           : k % 2 !== 0 && "bg-gray-200"
       )}
-    ></div>
+    >
+      {hasQueen && "***"}
+      <span className="absolute top-0 right-0 text-[8px] text-gray-500">
+        {j},{k}
+      </span>
+    </div>
   );
 };
 
